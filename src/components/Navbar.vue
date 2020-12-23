c64c22331659ec24a71f18fc0f8f78b45cb839d4
erictumuti <erictumuti114@gmail.com>
2020-12-23 13:37:26 +0300

    getting the current logged in user details and logging out user composables

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav v-if="user">
+	<p>Hey {{ user.displayName }}</p>
+	<p class="email">Currently logged in as {{ user.email }}</p>
+	<button @click="handleClick">logout</button>
+  </nav>
+</template>
+
+<script>
+import useLogout from '@/composables/useLogout'
+import getUser from '@/composables/getUser'
+export default {
+	setup() {
+		const { error, logout } = useLogout()
+
+		const { user } = getUser()
+
+		const handleClick = async () => {
+			await logout()
+			if(!error.value) {
+				console.log('user logged out')
+			}
+		}
+
+		return { handleClick, user }
+	}
+}
+</script>
+
+<style>
+
+</style>
