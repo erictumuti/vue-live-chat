0b4667230ca137cc6983d9657c3f74ad9cbed1eb
erictumuti <erictumuti114@gmail.com>
2020-12-23 11:21:33 +0300

    creating login composable and generating errors

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,23 +2,30 @@
   <form @submit.prevent="handleSubmit">
 	  <input type="email" required placeholder="your email" v-model="email">
 	  <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
 	  <button>Sign in</button>
   </form>
 </template>
 
 <script>
+import useLogin from '@/composables/useLogin'
 import { ref } from 'vue'
 export default {
 setup() {
 	// refs
 	const email = ref('')
-	const password = ref('')
+  const password = ref('')
 
-	const handleSubmit = () => {
-		console.log( email.value, password.value )
+  const { error, login } = useLogin()
+
+	const handleSubmit = async () => {
+    await login(email.value, password.value)
+    if(!error.value) {
+      console.log('user logged in')
+    }
 	}
 
-	return { email, password, handleSubmit }
+	return { email, password, handleSubmit, error }
 }
 }
 </script>
